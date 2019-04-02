c06760cec18e7600cbbd70515ad6a96c4f883611
Kazuya Ueoka <info@fromkk.info>
2019-04-02 10:33:17 +0900

    fixed sha256

--- testsummaries.rb
+++ testsummaries.rb
@@ -2,7 +2,7 @@ class Testsummaries < Formula
   desc "generate HTML file from TestSummaries.plist"
   homepage ""
   url "https://github.com/fromkk/TestSummaries/archive/0.0.11.tar.gz"
-  sha256 "6e31fb5051f96c7f7aece04d13819995eb56a969a21d9abaee59ed7104dc02ad"
+  sha256 "5c2dba82c5c1a1dd11b42781c5256a2c6554d33f6648a00093f1f6db705253d9"
   depends_on :xcode => ["10.2", :build]
 
   def install
