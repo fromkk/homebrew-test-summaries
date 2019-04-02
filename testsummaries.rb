b1a7dd9df42a5241a6a915a9374c0120cd7feaa1
Kazuya Ueoka <info@fromkk.info>
2019-04-02 11:10:43 +0900

    update to 0.0.12

--- testsummaries.rb
+++ testsummaries.rb
@@ -1,8 +1,8 @@
 class Testsummaries < Formula
   desc "generate HTML file from TestSummaries.plist"
   homepage ""
-  url "https://github.com/fromkk/TestSummaries/archive/0.0.11.tar.gz"
-  sha256 "5c2dba82c5c1a1dd11b42781c5256a2c6554d33f6648a00093f1f6db705253d9"
+  url "https://github.com/fromkk/TestSummaries/archive/0.0.12.tar.gz"
+  sha256 "c922f0d9ca3fc1e52621ffe0090c5218f58dfcd36aee7df23885d99175bf1bce"
   depends_on :xcode => ["10.2", :build]
 
   def install
