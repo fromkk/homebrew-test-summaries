e133beabf1b4d1ece9d2dec12b55ceab47579d57
Kazuya Ueoka <info@fromkk.info>
2019-04-20 16:11:17 +0900

    fixed hash

--- testsummaries.rb
+++ testsummaries.rb
@@ -2,7 +2,7 @@ class Testsummaries < Formula
   desc "generate HTML file from TestSummaries.plist"
   homepage ""
   url "https://github.com/fromkk/TestSummaries/archive/0.0.13.tar.gz"
-  sha256 "9ec9a814c19bc3bd328896b303ff31015a8037b035408910ad14ef7a13b40552"
+  sha256 "11d2193fa4422343750c313b966d9de617ecb2778b284f1fe11d4712c7f8de1f"
   depends_on :xcode => ["10.2", :build]
 
   def install
